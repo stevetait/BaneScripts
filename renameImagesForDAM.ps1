f9a7e6b7b5817ccd77b665c9b50fc855ac28212e
Steve - Tait Towers <steve@taittowers.com>
2012-07-11 15:29:35 -0400

    renameImages script now handles jpg, jpeg, gif, bmp, and png files

--- renameImagesForDAM.ps1
+++ renameImagesForDAM.ps1
@@ -13,33 +13,165 @@ Function renameFiles
 		# Set prefix to Show name - top level directory name
 		$showNamePrefix = $subDir
 		
-		$files = Get-ChildItem -Path $subDir.FullName -Filter *.jpg -Recurse
+		$jpgFiles = Get-ChildItem -Path $subDir.FullName -Filter *.jpg -Recurse
 		
-		Foreach ($file In $files)
+		Foreach ($jpgFile In $jpgFiles)
 				{
 				
 				# Check if a file exists
-				If ($file)
+				If ($jpgFile)
 					{
 					
-					$parentDirectory = Split-Path -leaf $file.DirectoryName
+					$parentDirectory = Split-Path -leaf $jpgFile.DirectoryName
 					
 					if($showNamePrefix.Name.CompareTo($parentDirectory))
 						{
-						$newFileName = "$showNamePrefix-$parentDirectory-$file"
+						$newFileName = "$showNamePrefix-$parentDirectory-$jpgFile"
 						}
 					else
 						{
-						$newFileName = "$showNamePrefix-$file"
+						$newFileName = "$showNamePrefix-$jpgFile"
 						}
 					
 					
 					$newFileName -replace " ","_"
 					
 					# Trim spaces and rename the file
-					$image_string = $file.fullname.ToString().Trim()
+					$image_string = $jpgFile.fullname.ToString().Trim()
 					
-					# Rename $file to $newFileName
+					# Rename $jpgFile to $newFileName
+					Rename-Item "$image_string" "$newFileName"
+					
+					}
+				
+				}
+		
+		$jpegFiles = Get-ChildItem -Path $subDir.FullName -Filter *.jpeg -Recurse
+		
+		Foreach ($jpegFile In $jpegFiles)
+				{
+				
+				# Check if a file exists
+				If ($jpegFile)
+					{
+					
+					$parentDirectory = Split-Path -leaf $jpegFile.DirectoryName
+					
+					if($showNamePrefix.Name.CompareTo($parentDirectory))
+						{
+						$newFileName = "$showNamePrefix-$parentDirectory-$jpegFile"
+						}
+					else
+						{
+						$newFileName = "$showNamePrefix-$jpegFile"
+						}
+					
+					
+					$newFileName -replace " ","_"
+					
+					# Trim spaces and rename the file
+					$image_string = $jpegFile.fullname.ToString().Trim()
+					
+					# Rename $jpegFile to $newFileName
+					Rename-Item "$image_string" "$newFileName"
+					
+					}
+				
+				}
+		
+		$gifFiles = Get-ChildItem -Path $subDir.FullName -Filter *.gif -Recurse
+		
+		Foreach ($gifFile In $gifFiles)
+				{
+				
+				# Check if a file exists
+				If ($gifFile)
+					{
+					
+					$parentDirectory = Split-Path -leaf $gifFile.DirectoryName
+					
+					if($showNamePrefix.Name.CompareTo($parentDirectory))
+						{
+						$newFileName = "$showNamePrefix-$parentDirectory-$gifFile"
+						}
+					else
+						{
+						$newFileName = "$showNamePrefix-$gifFile"
+						}
+					
+					
+					$newFileName -replace " ","_"
+					
+					# Trim spaces and rename the file
+					$image_string = $gifFile.fullname.ToString().Trim()
+					
+					# Rename $gifFile to $newFileName
+					Rename-Item "$image_string" "$newFileName"
+					
+					}
+				
+				}
+		
+		$bmpFiles = Get-ChildItem -Path $subDir.FullName -Filter *.bmp -Recurse
+		
+		Foreach ($bmpFile In $bmpFiles)
+				{
+				
+				# Check if a file exists
+				If ($bmpFile)
+					{
+					
+					$parentDirectory = Split-Path -leaf $bmpFile.DirectoryName
+					
+					if($showNamePrefix.Name.CompareTo($parentDirectory))
+						{
+						$newFileName = "$showNamePrefix-$parentDirectory-$bmpFile"
+						}
+					else
+						{
+						$newFileName = "$showNamePrefix-$bmpFile"
+						}
+					
+					
+					$newFileName -replace " ","_"
+					
+					# Trim spaces and rename the file
+					$image_string = $bmpFile.fullname.ToString().Trim()
+					
+					# Rename $bmpFile to $newFileName
+					Rename-Item "$image_string" "$newFileName"
+					
+					}
+				
+				}
+		
+		$pngFiles = Get-ChildItem -Path $subDir.FullName -Filter *.png -Recurse
+		
+		Foreach ($pngFile In $pngFiles)
+				{
+				
+				# Check if a file exists
+				If ($pngFile)
+					{
+					
+					$parentDirectory = Split-Path -leaf $pngFile.DirectoryName
+					
+					if($showNamePrefix.Name.CompareTo($parentDirectory))
+						{
+						$newFileName = "$showNamePrefix-$parentDirectory-$pngFile"
+						}
+					else
+						{
+						$newFileName = "$showNamePrefix-$pngFile"
+						}
+					
+					
+					$newFileName -replace " ","_"
+					
+					# Trim spaces and rename the file
+					$image_string = $pngFile.fullname.ToString().Trim()
+					
+					# Rename $pngFile to $newFileName
 					Rename-Item "$image_string" "$newFileName"
 					
 					}
