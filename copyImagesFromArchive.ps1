acfdb18839646c101f8a2d32dce3f6325b2ac600
Steve - Tait Towers <steve@taittowers.com>
2012-08-29 15:43:50 -0400

    Added test if grandchild dirs exist

--- copyImagesFromArchive.ps1
+++ copyImagesFromArchive.ps1
@@ -10,7 +10,7 @@ Function moveShowfileImages
 	#Param($filter)
 	
 	#Set working directory to showfile
-	Set-Location "\\FS01\ARCHIVE"
+	Set-Location "D:\DAMworking"
 	
 	$parentDir = [Environment]::CurrentDirectory=(Get-Location -PSProvider FileSystem).ProviderPath
 	#"Parent Directory: $parentDir"
@@ -30,53 +30,55 @@ Function moveShowfileImages
 		# Gets all directories below the current directory
 		$grandChildDirs = dir | Where { $_.psIsContainer -eq $true }
 		
-		Foreach ($grandChildDir In $grandChildDirs)
+		if($grandChildDirs -gt 0)
 			{
-			# Get full path of show folder
-			Set-Location $grandChildDir.FullName
-			
-			$currentGrandChildDir = [Environment]::CurrentDirectory=(Get-Location -PSProvider FileSystem).ProviderPath
-		
-			#"          Grandchild Directory: $currentGrandChildDir"
-			
-			# Append \IMAGES\JPEG subfolder to path
-			$imagesJpegFolderPath = Join-Path $grandChildDir.FullName "\IMAGES\JPEG"
-			
-			# Append \JPEGS subfolder to path
-			$jpegsFolderPath = Join-Path $grandChildDir.FullName "\JPEGS"
-			
-			if(Test-Path $imagesJpegFolderPath)
+			Foreach ($grandChildDir In $grandChildDirs)
 				{
-				#"$imagesJpegFolderPath"
+				# Get full path of show folder
+				Set-Location $grandChildDir.FullName
 				
-				# Create Output Path
-				$outputPath = Join-Path "X:\PhotosForImport\ARCHIVE" $childDir.Name
+				$currentGrandChildDir = [Environment]::CurrentDirectory=(Get-Location -PSProvider FileSystem).ProviderPath
+			
+				#"          Grandchild Directory: $currentGrandChildDir"
 				
-				$outputPath = Join-Path $outputPath $grandChildDir
+				# Append \IMAGES\JPEG subfolder to path
+				$imagesJpegFolderPath = Join-Path $grandChildDir.FullName "\IMAGES\JPEG"
 				
-				# Debug - output $outputPath
-				"$outputPath"
+				# Append \JPEGS subfolder to path
+				$jpegsFolderPath = Join-Path $grandChildDir.FullName "\JPEGS"
 				
-				get-childitem "$imagesJpegFolderPath" -include @("*.jpg","*.jpeg","*.bmp","*.gif","*.png") -recurse | copy-item -destination {$newpath=$_.PSParentPath -replace [regex]::escape($imagesJpegFolderPath), "$outputPath"; new-item -path $newpath -type directory -ea SilentlyContinue | out-null; write-output $newpath}
-				}
-			elseif(Test-Path $jpegsFolderPath)
-				{
-				#"$jpegsFolderPath"
-				
-				# Create Output Path
-				$outputPath = Join-Path "X:\PhotosForImport\ARCHIVE" $childDir.Name
-				
-				$outputPath = Join-Path $outputPath $grandChildDir
-				
-				# Debug - output $outputPath
-				"$outputPath"
-				
-				get-childitem "$jpegsFolderPath" -include @("*.jpg","*.jpeg","*.bmp","*.gif","*.png") -recurse | copy-item -destination {$newpath=$_.PSParentPath -replace [regex]::escape($jpegsFolderPath), "$outputPath"; new-item -path $newpath -type directory -ea SilentlyContinue | out-null; write-output $newpath}
-				}
+				if(Test-Path $imagesJpegFolderPath)
+					{
+					#"$imagesJpegFolderPath"
+					
+					# Create Output Path
+					$outputPath = Join-Path "X:\PhotosForImport\ARCHIVE" $childDir.Name
+					
+					$outputPath = Join-Path $outputPath $grandChildDir
+					
+					# Debug - output $outputPath
+					"$outputPath"
+					
+					get-childitem "$imagesJpegFolderPath" -include @("*.jpg","*.jpeg","*.bmp","*.gif","*.png") -recurse | copy-item -destination {$newpath=$_.PSParentPath -replace [regex]::escape($imagesJpegFolderPath), "$outputPath"; new-item -path $newpath -type directory -ea SilentlyContinue | out-null; write-output $newpath}
+					}
+				elseif(Test-Path $jpegsFolderPath)
+					{
+					#"$jpegsFolderPath"
+					
+					# Create Output Path
+					$outputPath = Join-Path "X:\PhotosForImport\ARCHIVE" $childDir.Name
+					
+					$outputPath = Join-Path $outputPath $grandChildDir
+					
+					# Debug - output $outputPath
+					"$outputPath"
+					
+					get-childitem "$jpegsFolderPath" -include @("*.jpg","*.jpeg","*.bmp","*.gif","*.png") -recurse | copy-item -destination {$newpath=$_.PSParentPath -replace [regex]::escape($jpegsFolderPath), "$outputPath"; new-item -path $newpath -type directory -ea SilentlyContinue | out-null; write-output $newpath}
+					}
 
+				}
 			}
 		}
-	
 	}
 
 # Run script
